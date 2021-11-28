313d5eec740033ccc64a899a56256c6d51f94959
안승우 <rio319@naver.com>
2021-11-28 23:40:02 +0900

    UI imsi

--- app/src/main/java/com/example/studybridge/Mypage/MyPageFragment.java
+++ app/src/main/java/com/example/studybridge/Mypage/MyPageFragment.java
@@ -9,13 +9,44 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.studybridge.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class MyPageFragment extends Fragment {
+    private TabLayout tabLayout;
+    private MyPageMyFragment myFragment;
+    private MyPageChatFragment chatFragment;
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.mypage_fragment, container, false);
+        View view = inflater.inflate(R.layout.mypage_fragment, container, false);
+        tabLayout = (TabLayout) view.findViewById(R.id.mypage_tab);
+
+        myFragment = new MyPageMyFragment();
+        chatFragment = new MyPageChatFragment();
+
+        getParentFragmentManager().beginTransaction().add(R.id.mypage_frame,myFragment).commit();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Fragment selected = null;
+                if(position == 0){
+                    selected = myFragment;
+                } else if(position == 1) {
+                    selected = chatFragment;
+                }
+                getParentFragmentManager().beginTransaction().replace(R.id.mypage_frame,selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/studybridge/Mypage/MyPageMyFragment.java
@@ -0,0 +1,23 @@
+package com.example.studybridge.Mypage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.studybridge.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MyPageMyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.mypage_my_fragment,container,false);
+        return view;
+    }
+}
